daf6067b86ff9547c0b0098f73f7ea39c226e292
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-11 20:39:55 +0530

    added mobile navbar and desktop navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
-
+import Navbar from './components/Navbar';
+import MobileMenu from './components/MobileMenu';
 import LoadingScreen from "./components/LoadingScreen";
 import './index.css';
 
 function App() {
   const [isLoading,setIsLoading] = useState(false);
+   const [menuOpen,setMenuOpen] = useState(false);
 
   return (
     <>
       {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)}/>}
       {' '}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
-
+      <div className={`min-h-screen transition-opacity duration-700 
+          ${isLoading ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   )
